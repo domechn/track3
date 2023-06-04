a4537b06e0aee2a40993689a23b41894e3a9664b
domchan <31119455+domechn@users.noreply.github.com>
2023-06-04 22:39:29 +0800

    add a new provider for doge coin (#39)

--- src/middlelayers/datafetch/coins/doge.ts
+++ src/middlelayers/datafetch/coins/doge.ts
@@ -3,22 +3,33 @@ import _ from 'lodash'
 import { asyncMap } from '../utils/async'
 import { sendHttpRequest } from '../utils/http'
 
+interface DogeQuerier {
+	query(address: string): Promise<number>
+}
+
 export class DOGEAnalyzer implements Analyzer {
 	private readonly config: Pick<TokenConfig, 'doge'>
 
-	private readonly queryUrl = "https://dogechain.info/api/v1/address/balance/"
+	private dogeQueriers: DogeQuerier[]
 
 	constructor(config: Pick<TokenConfig, 'doge'>) {
 		this.config = config
+		this.dogeQueriers = [new DogeInfo(), new BlockCypher()]
 	}
 	getAnalyzeName(): string {
 		return "DOGE Analyzer"
 	}
 
 	private async query(address: string): Promise<number> {
-		const resp = await sendHttpRequest<{ balance: number }>("GET", this.queryUrl + address)
-		const amount = _(resp.balance).toNumber()
-		return amount
+		for (const q of this.dogeQueriers) {
+			try {
+				return q.query(address)
+			}
+			catch (e) {
+				console.error(e)
+			}
+		}
+		throw new Error("All DOGE queriers failed")
 	}
 
 	async loadPortfolio(): Promise<Coin[]> {
@@ -27,5 +38,25 @@ export class DOGEAnalyzer implements Analyzer {
 			symbol: "DOGE",
 			amount: _(coinLists).sum(),
 		}]
+	}
+}
+
+class DogeInfo implements DogeQuerier {
+	private readonly queryUrl = "https://dogechain.info/api/v1/address/balance/"
+
+	async query(address: string): Promise<number> {
+		const resp = await sendHttpRequest<{ balance: number }>("GET", this.queryUrl + address)
+		const amount = _(resp.balance).toNumber()
+		return amount
+	}
+}
+
+class BlockCypher implements DogeQuerier {
+	private readonly queryUrl = "https://api.blockcypher.com/v1/doge/main/addrs/"
+
+	async query(address: string): Promise<number> {
+		const resp = await sendHttpRequest<{ final_balance: number }>("GET", this.queryUrl + address)
+		const amount = _(resp.final_balance).toNumber() / 1e8
+		return amount
 	}
 }
