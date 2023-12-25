d2915e8a550db6e54d80c1778ebe55a734ab0c53
domchan <31119455+domechn@users.noreply.github.com>
2023-12-26 00:02:15 +0800

    update wallet to non null value (#220)

--- src-tauri/migrations/v04t05/assets_v2_uniq_idx_up.sql
+++ src-tauri/migrations/v04t05/assets_v2_uniq_idx_up.sql
@@ -1,1 +1,2 @@
 CREATE UNIQUE INDEX IF NOT EXISTS unique_uuid_symbol_amount_wallet ON assets_v2 (uuid, symbol, wallet);
+UPDATE assets_v2 SET wallet = '' WHERE wallet IS NULL; -- update legacy data's wallet
