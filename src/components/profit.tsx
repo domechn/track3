4a5ab0acaf72f349a20f095be3df7ec8d922dbad
domchan <31119455+domechn@users.noreply.github.com>
2024-01-06 13:49:15 +0800

    add table to list coins profit (#239)

--- src/components/profit.tsx
+++ src/components/profit.tsx
@@ -1,9 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { CurrencyRateDetail } from "@/middlelayers/types";
 import { currencyWrapper, prettyNumberToLocaleString } from "@/utils/currency";
 import { calculateTotalProfit } from "@/middlelayers/charts";
+import { appCacheDir as getAppCacheDir } from "@tauri-apps/api/path";
 import { loadingWrapper } from "@/utils/loading";
+import { Table, TableBody, TableCell, TableRow } from "./ui/table";
+import UnknownLogo from "@/assets/icons/unknown-logo.svg";
+import _ from "lodash";
+import { Skeleton } from "./ui/skeleton";
+import bluebird from "bluebird";
+import { getImageApiPath } from "@/utils/app";
+import { ButtonGroup, ButtonGroupItem } from "./ui/button-group";
+
+type TopType = "profitTop" | "lossTop";
 
 const App = ({
   currency,
@@ -14,6 +24,14 @@ const App = ({
   version: number;
 }) => {
   const [profit, setProfit] = useState(0);
+  const [coinsProfit, setCoinsProfit] = useState<
+    {
+      symbol: string;
+      value: number;
+    }[]
+  >([]);
+  const [logoMap, setLogoMap] = useState<{ [x: string]: string }>({});
+  const [topType, setTopType] = useState<TopType>("profitTop");
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -21,9 +39,44 @@ const App = ({
     calculateTotalProfit()
       .then((res) => {
         setProfit(res.total);
+        setCoinsProfit(_(res.coins).sortBy("value").value());
+
+        // set logo map
+        getLogoMap(res.coins).then((m) => setLogoMap(m));
       })
       .finally(() => setLoading(false));
   }, [version]);
+
+  const topTypeData = useMemo(() => {
+    const size = 5;
+
+    return topType === "profitTop"
+        ? _(coinsProfit).takeRight(size).reverse().value()
+        : _(coinsProfit).take(5).value();
+
+  }, [coinsProfit, topType]);
+
+  async function getLogoMap(d: { symbol: string }[]) {
+    const acd = await getAppCacheDir();
+    const kvs = await bluebird.map(d, async (coin) => {
+      const path = await getImageApiPath(acd, coin.symbol);
+      return { [coin.symbol]: path };
+    });
+
+    return _.assign({}, ...kvs);
+  }
+
+  function onTypeSelectChange(val: TopType) {
+    setTopType(val);
+  }
+
+  function getProfitDisplayColor(val: number) {
+    if (val === 0) {
+      return "gray";
+    }
+
+    return val > 0 ? "green" : "red";
+  }
 
   return (
     <div>
@@ -42,6 +95,56 @@ const App = ({
             </div>,
             "h-[32px]"
           )}
+
+          <ButtonGroup
+            defaultValue="profitTop"
+            onValueChange={(val: TopType) => onTypeSelectChange(val)}
+          >
+            <ButtonGroupItem value="profitTop">Profit Top</ButtonGroupItem>
+            <ButtonGroupItem value="lossTop">Loss Top</ButtonGroupItem>
+          </ButtonGroup>
+
+          <Table>
+            <TableBody>
+              {loading
+                ? _(5)
+                    .range()
+                    .map((i) => (
+                      <TableRow key={"coin-profit-row-loading-" + i}>
+                        <TableCell>
+                          <Skeleton className="my-[10px] h-[20px] w-[100%]" />
+                        </TableCell>
+                      </TableRow>
+                    ))
+                    .value()
+                : topTypeData.map((d) => (
+                    <TableRow key={d.symbol} className="h-[55px]">
+                      <TableCell>
+                        <div className="flex flex-row items-center">
+                          <img
+                            className="inline-block w-[20px] h-[20px] mr-2 rounded-full"
+                            src={logoMap[d.symbol] || UnknownLogo}
+                            alt={d.symbol}
+                          />
+                          <div className="font-bold text-base">{d.symbol}</div>
+                        </div>
+                      </TableCell>
+                      <TableCell className="text-right">
+                        <div
+                          className={`text-${getProfitDisplayColor(
+                            d.value
+                          )}-600`}
+                        >
+                          {currency.symbol +
+                            prettyNumberToLocaleString(
+                              currencyWrapper(currency)(d.value)
+                            )}
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+            </TableBody>
+          </Table>
         </CardContent>
       </Card>
     </div>
