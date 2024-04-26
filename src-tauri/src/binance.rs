3583afcf4d50149824980a7c525430dd5e4666b6
domchan <31119455+domechn@users.noreply.github.com>
2024-04-26 23:08:37 +0300

    support lising data in binance locked savings (#382)

--- src-tauri/src/binance.rs
+++ src-tauri/src/binance.rs
@@ -1,11 +1,23 @@
 use binance::{
-    account::Account, api::Binance as BinanceApi, futures::account::FuturesAccount, wallet::Wallet,
+    account::Account, api::Binance as BinanceApi, futures::account::FuturesAccount,
+    util::build_signed_request, wallet::Wallet,
 };
 use std::collections::HashMap;
 
 pub struct Binance {
     api_key: String,
     secret_key: String,
+}
+
+#[derive(Debug, serde::Deserialize)]
+struct LockedSavingResponse {
+    rows: Vec<LockedSavingRowItem>,
+}
+
+#[derive(Debug, serde::Deserialize)]
+struct LockedSavingRowItem {
+    asset: String,
+    amount: String,
 }
 
 impl Binance {
@@ -34,7 +46,7 @@ impl Binance {
                 acc
             });
 
-        // funding
+        // funding: it includes flexible savings
         let wallet_funding = funding.funding_wallet(None, None).await?;
         wallet_funding
             .iter()
@@ -47,6 +59,28 @@ impl Binance {
                     res.insert(k.to_owned(), v);
                 }
             });
+
+        // locked savings in simple earn
+        let locked_savings_req = build_signed_request([("size", "100")], funding.recv_window)?;
+        let locked_savings: LockedSavingResponse = funding
+            .client
+            .get_signed(
+                "/sapi/v1/simple-earn/locked/position",
+                locked_savings_req.as_str(),
+            )
+            .await?;
+        print!("{:?}", locked_savings);
+        
+        locked_savings.rows.iter().for_each(|row| {
+            let asset = row.asset.clone();
+            let amount = row.amount.parse::<f64>().unwrap_or(0.0);
+            if res.contains_key(&asset) {
+                let val = res.get(&asset).unwrap();
+                res.insert(asset.to_owned(), val + amount);
+            } else {
+                res.insert(asset.to_owned(), amount);
+            }
+        });
 
         // futures
         let futures_balances = futures.account_balance().await?;
